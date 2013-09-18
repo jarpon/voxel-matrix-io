ee67f9a6301688ef98f9312edd3a11c7b50bd792
Javier Arpon <javier.arpon@gmail.com>
2013-09-18 11:57:24 +0200

    VoxelMatrix Batch Reader: this lets the user to open a directory of VoxelMatrix files

---
+++ src/main/java/VoxelMatrix_Batch_Reader.java
@@ -0,0 +1,63 @@
+import ij.plugin.PlugIn;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JFileChooser;
+import ij.ImagePlus;
+
+/*///////////////////////////////////////
+This plugin opens vm files found into a folder.
+To change this to be able to open multiple files, ""of.setFileSelectionMode(JFileChooser.FILES_ONLY)""; has to be commented out
+///////////////////////////////////////*/
+
+
+public class VoxelMatrix_Batch_Reader implements PlugIn{ 
+	
+	public static ImagePlus imp;
+	
+	public void run(String arg) 
+	{
+		String inputFolder = null;
+
+		//get the folder of the original images
+		JFileChooser of = new JFileChooser();
+		of.setDialogTitle("Select the folder of .vm files you want to open...");
+		//of.setFileSelectionMode(JFileChooser.FILES_ONLY);   // default of class
+   		of.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int retval = of.showOpenDialog(null);
+
+		//process both directories
+		if ( retval == JFileChooser.APPROVE_OPTION ) {
+			//File nFolder = nf.getCurrentDirectory();//it's not necessary
+			inputFolder = of.getSelectedFile().getAbsolutePath();		   
+			//IJ.log(inputFolder);
+			// DO YOUR PROCESSING HERE. OPEN FILE OR ...
+
+			listRecursive( inputFolder );
+		}
+		else return;
+	}
+	   
+	public static <ImagePlus> void listRecursive( String inputDir ) 
+	{
+		File[] filesList = new File(inputDir).listFiles();
+		String[] originalList = new File(inputDir).list();
+		//File[] nucleusList = new File(nucleusDir).listFiles(); //only is necessary the original number
+
+		int totalImages = (int)originalList.length;
+		for (int i=0; i < totalImages; ++i) 
+		{
+			String path = filesList[i].toString();
+	    		VoxelMatrix_Reader vm = new VoxelMatrix_Reader();
+    			try {
+					imp = vm.read_it(path);
+					imp.show();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+    			
+		}
+ 	}
+ 	
+}
