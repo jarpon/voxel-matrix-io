21d9170fe0ae4be7d0e6e3cd0d51cf5dbf8fe595
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2014-09-02 12:05:59 +0200

    Add overlay to original image based on segmented image.
    The LUT is based on the golden angle and it skips the first index so the first color is black and the second is red.

--- src/main/java/inra/ijpb/voxelmatrix/Segmentation_Checker.java
+++ src/main/java/inra/ijpb/voxelmatrix/Segmentation_Checker.java
@@ -1,11 +1,20 @@
 package inra.ijpb.voxelmatrix;
 
+import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -15,8 +24,11 @@ import javax.swing.SwingUtilities;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.gui.ImageCanvas;
+import ij.gui.ImageRoi;
+import ij.gui.Overlay;
 import ij.gui.StackWindow;
 import ij.plugin.PlugIn;
+import ij.process.LUT;
 import fiji.util.gui.GenericDialogPlus;
 
 public class Segmentation_Checker implements PlugIn 
@@ -34,12 +46,26 @@ public class Segmentation_Checker implements PlugIn
 	/** current image name */
 	String currentImageName = null;
 	
+	/** current original image */
+	ImagePlus originalImage = null;
+	/** current segmented image */
+	ImagePlus segmentedImage = null;
+	
+	/** current transparency */
+	Float transparency = 0.33f;
+	
+	/** overlay LUT */
+	LUT overlayLUT = Segmentation_Checker.createGoldenAngleLut();
+	
 	File[] originalFilesList = null;
 	File[] segmentedFilesList = null;
 	ArrayList<String> discardedFilesList = null;
 	
 	/** main GUI window */
 	private CustomWindow win;
+	
+	/** executor service to launch threads for the plugin methods and events */
+	final ExecutorService exec = Executors.newFixedThreadPool(1);
 
 	// Button panel components
 	JButton validateButton = null;
@@ -62,7 +88,29 @@ public class Segmentation_Checker implements PlugIn
 		{
 		
 			super(imp, new ImageCanvas(imp));
+			
+			// assign original image
+			originalImage = imp;
+			// read corresponding segmented image
+			try{				
+				segmentedImage = currentImageName.endsWith( ".vm" ) ?
+					reader.readIt( segmentedFilesList[counter].getParent().toString() + "/" + currentImageName ) :
+					new ImagePlus( segmentedFilesList[counter].getParent().toString() + "/" + currentImageName );
+			
+			}catch( Exception ex ){
+				IJ.error("Could not load " + segmentedFilesList[counter].getParent().toString() + "/" + currentImageName);
+				ex.printStackTrace();
+				return;
+			}
 
+			// assign LUT to segmented image
+			segmentedImage.getProcessor().setColorModel( overlayLUT );
+			segmentedImage.getImageStack().setColorModel( overlayLUT );
+			segmentedImage.setDisplayRange( 0, 255 );
+			segmentedImage.updateAndDraw();
+			
+			//segmentedImage.show();
+			
 			final ImageCanvas canvas = (ImageCanvas) getCanvas();
 
 			// Zoom in if image is too small
@@ -150,6 +198,89 @@ public class Segmentation_Checker implements PlugIn
 			pack();
 			setMinimumSize( getPreferredSize() );
 			
+			// add especial listener if the input image is a stack
+			if(null != sliceSelector)
+			{
+				// add adjustment listener to the scroll bar
+				sliceSelector.addAdjustmentListener(new AdjustmentListener() 
+				{
+
+					public void adjustmentValueChanged(final AdjustmentEvent e) {
+						exec.submit(new Runnable() {
+							public void run() {							
+								if(e.getSource() == sliceSelector)
+								{
+									//IJ.log("moving scroll");
+									originalImage.killRoi();									
+									updateOverlay();
+									originalImage.updateAndDraw();									
+								}
+							}							
+						});
+					}
+				});
+
+				// mouse wheel listener to update the rois while scrolling
+				addMouseWheelListener(new MouseWheelListener() {
+
+					@Override
+					public void mouseWheelMoved(final MouseWheelEvent e) {
+
+						exec.submit(new Runnable() {
+							public void run() 
+							{
+								//IJ.log("moving scroll");
+								originalImage.killRoi();									
+								updateOverlay();
+								originalImage.updateAndDraw();
+							}
+						});
+
+					}
+				});
+
+				// key listener to repaint the display image and the traces
+				// when using the keys to scroll the stack
+				KeyListener keyListener = new KeyListener() {
+
+					@Override
+					public void keyTyped(KeyEvent e) {}
+
+					@Override
+					public void keyReleased(final KeyEvent e) {
+						exec.submit(new Runnable() {
+							public void run() 
+							{
+								if(e.getKeyCode() == KeyEvent.VK_LEFT ||
+										e.getKeyCode() == KeyEvent.VK_RIGHT ||
+										e.getKeyCode() == KeyEvent.VK_LESS ||
+										e.getKeyCode() == KeyEvent.VK_GREATER ||
+										e.getKeyCode() == KeyEvent.VK_COMMA ||
+										e.getKeyCode() == KeyEvent.VK_PERIOD)
+								{
+									//IJ.log("moving scroll");
+									originalImage.killRoi();									
+									updateOverlay();
+									originalImage.updateAndDraw();
+								}
+							}
+						});
+
+					}
+
+					@Override
+					public void keyPressed(KeyEvent e) {}
+				};
+				// add key listener to the window and the canvas
+				addKeyListener(keyListener);
+				canvas.addKeyListener(keyListener);
+
+			}	
+			
+			// update display image
+			updateOverlay();
+			originalImage.updateAndDraw();
+			
 		}
 
 		/**
@@ -159,8 +290,71 @@ public class Segmentation_Checker implements PlugIn
 		
 		
 		
-	}
+	}// end class CustomWindow
+	
+	
+		
+	/**
+	 * Update the overlay in the display image based on 
+	 * the current segmentation image and slice
+	 */
+	void updateOverlay() 
+	{
+		if( null != segmentedImage )
+		{
+			originalImage.deleteRoi();
+			int slice = originalImage.getCurrentSlice();
 
+			// create image ROI with segmented image and current transparency
+			ImageRoi roi = new ImageRoi(0, 0, segmentedImage.getImageStack().getProcessor( slice ) );
+			roi.setOpacity( 1.0 - transparency );
+			
+			originalImage.setOverlay( new Overlay( roi ) );
+		}
+	}
+	
+	/**
+	 * Create golden angle LUT (skipping first index, 
+	 * which is black).
+	 * @return golden angle LUT
+	 */
+	public final static LUT createGoldenAngleLut() 
+	{
+		// Create overlay LUT
+		final byte[] red = new byte[ 256 ];
+		final byte[] green = new byte[ 256 ];
+		final byte[] blue = new byte[ 256 ];
+
+		// hue for assigning new color ([0.0-1.0])
+		float hue = 0f;
+		// saturation for assigning new color ([0.5-1.0]) 
+		float saturation = 1f; 
+		Color[] colors = new Color[ 254 ]; 
+		for(int i=0; i<254; i++)
+		{
+			colors[ i ] = Color.getHSBColor(hue, saturation, 1);
+
+			hue += 0.38197f; // golden angle
+			if (hue > 1) 
+				hue -= 1;
+			saturation += 0.38197f; // golden angle
+			if (saturation > 1)
+				saturation -= 1;
+			saturation = 0.5f * saturation + 0.5f;							
+		}
+							
+		for(int i = 1 ; i < 255; i++)
+		{
+			//IJ.log("i = " + i + " color index = " + colorIndex);
+			red[i] = (byte) colors[ i-1 ].getRed();
+			green[i] = (byte) colors[ i-1 ].getGreen();
+			blue[i] = (byte) colors[ i-1 ].getBlue();
+		}
+		
+		return new LUT(red, green, blue);
+	}
+	
+	
 	//---------------------------------------------------------------------------------
 	/**
 	 * Plug-in run method
@@ -206,7 +400,7 @@ public class Segmentation_Checker implements PlugIn
 		// sort file names
 		Arrays.sort( segmentedFilesList );
 		
-		String currentImageName = getNextImageName();
+		currentImageName = getNextImageName();
 		
 		if( null == currentImageName )
 			return;
