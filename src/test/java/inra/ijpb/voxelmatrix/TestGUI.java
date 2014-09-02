91faed8655bf3965a8c10199f5d6c0fb9310b18c
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2014-09-02 10:39:46 +0200

    Add test class to launch Segmentation Checker in debug mode.

---
+++ src/test/java/inra/ijpb/voxelmatrix/TestGUI.java
@@ -0,0 +1,17 @@
+package inra.ijpb.voxelmatrix;
+
+import fiji.Debug;
+
+public class TestGUI {
+	/**
+	 * Main method to test and debug the Segmentation Checker
+	 *  
+	 * @param args
+	 */
+	public static void main( final String[] args )
+	{		
+		// Call the plugin with empty arguments (this
+		// will pop up an Open dialog)		
+		Debug.run("Segmentation Checker", "");
+	}
+}
